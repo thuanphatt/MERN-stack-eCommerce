b556da11da7989cbcdf67b1a3555e87e0ee9ddc4
thuanphatt <thphatt@gmail.com>
2023-08-07 21:31:43 +0700

    config mongodb

---
+++ server/config/dbconnect.js
@@ -0,0 +1,13 @@
+const { default: mongoose } = require("mongoose");
+const dbConnect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    if (conn.connection.readyState === 1)
+      console.log("DB connection secessfully");
+    else console.log("DB connecting");
+  } catch (error) {
+    console.log("DB Connection error");
+    throw new Error(error);
+  }
+};
+module.exports = dbConnect;
