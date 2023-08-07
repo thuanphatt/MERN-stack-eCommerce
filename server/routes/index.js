6081c0eda3f981ee4453b6bfff9dcca2cd597f48
thuanphatt <thphatt@gmail.com>
2023-08-07 21:33:48 +0700

    set up route

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const userRouter = require("./user");
+const { notFound, errorHandler } = require("../middlewares/errorHandler");
+const initRoutes = (app) => {
+  app.use("/api/user", userRouter);
+
+  app.use(notFound);
+  app.use(errorHandler);
+};
+module.exports = initRoutes;
