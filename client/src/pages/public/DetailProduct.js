d6f4976093c80f664a7bcea190dd4ca00dee0938
thuanphatt <thphatt@gmail.com>
2023-09-27 23:42:51 +0700

    detail product section #1

--- client/src/pages/public/DetailProduct.js
+++ client/src/pages/public/DetailProduct.js
@@ -1,30 +1,61 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Slider from "react-slick";
 
 import { apiGetProduct } from "../../apis/product";
 import { Breakcrumb } from "../../components";
-
+var settings = {
+	dots: false,
+	infinite: false,
+	speed: 500,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+};
 const DetailProduct = () => {
-  const { pid, title, category } = useParams();
-  const [product, setProduct] = useState(null);
-  const fetchProductData = async () => {
-    const response = await apiGetProduct(pid);
-    if (response.success) setProduct(response.productData);
-  };
-  useEffect(() => {
-    if (pid) fetchProductData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pid]);
-  return (
-    <div className="w-full">
-      <div className="h-[81px] bg-gray-100 flex justify-center items-center">
-        <div className="w-main">
-          <h3> {title}</h3>
-          <Breakcrumb title={title} category={category} />
-        </div>
-      </div>
-    </div>
-  );
+	const { pid, title, category } = useParams();
+	// eslint-disable-next-line no-unused-vars
+	const [product, setProduct] = useState(null);
+	const fetchProductData = async () => {
+		const response = await apiGetProduct(pid);
+		if (response.success) setProduct(response.productData);
+	};
+	useEffect(() => {
+		if (pid) fetchProductData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [pid]);
+	return (
+		<div className="w-full">
+			<div className="h-[81px] bg-gray-100 flex justify-center items-center">
+				<div className="w-main">
+					<h3> {title}</h3>
+					<Breakcrumb title={title} category={category} />
+				</div>
+			</div>
+			<div className="w-main m-auto mt-5 flex">
+				<div className="w-2/5 flex flex-col gap-4">
+					<img
+						src={product?.images[0]}
+						alt={product?.title}
+						className="h-[458px] w-[485px] border object-cover"
+					/>
+					<div className="w-[485px]">
+						<Slider {...settings}>
+							{product?.images.map((el) => (
+								<img
+									src={el}
+									alt="sub-product"
+									className="h-[143px] w-[143px] border object-cover"
+								/>
+							))}
+						</Slider>
+					</div>
+				</div>
+				<div className="w-2/5">price</div>
+				<div className="w-1/5">infomation</div>
+			</div>
+			<div className="h-[800px] w-full"></div>
+		</div>
+	);
 };
 
 export default DetailProduct;
--- server/controllers/product.js
+++ server/controllers/product.js
@@ -2,165 +2,165 @@ const Product = require("../models/product");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
 const createProduct = asyncHandler(async (req, res) => {
-  if (Object.keys(req.body).length === 0) throw new Error("Missing inputs");
-  if (req.body && req.body.title) req.body.slug = slugify(req.body.title);
-  const newProduct = await Product.create(req.body);
-  return res.status(200).json({
-    sucess: newProduct ? true : false,
-    created: newProduct ? newProduct : "No create product",
-  });
+	if (Object.keys(req.body).length === 0) throw new Error("Missing inputs");
+	if (req.body && req.body.title) req.body.slug = slugify(req.body.title);
+	const newProduct = await Product.create(req.body);
+	return res.status(200).json({
+		success: newProduct ? true : false,
+		created: newProduct ? newProduct : "No create product",
+	});
 });
 const getProduct = asyncHandler(async (req, res) => {
-  const { pid } = req.params;
-  const product = await Product.findById(pid);
-  return res.status(200).json({
-    sucess: product ? true : false,
-    productData: product ? product : "Cannot get product",
-  });
+	const { pid } = req.params;
+	const product = await Product.findById(pid);
+	return res.status(200).json({
+		success: product ? true : false,
+		productData: product ? product : "Cannot get product",
+	});
 });
 // Filter, sort and pagination product
 const getAllProduct = asyncHandler(async (req, res) => {
-  const queries = { ...req.query };
-  // Exclude special fields out of query
-  const excludeFields = ["limit", "sort", "page", "fields"];
-  excludeFields.forEach((el) => delete queries[el]);
-  // Format operators for right syntax of MongoDB
-  let queryString = JSON.stringify(queries);
-  queryString = queryString.replace(
-    /\b(gte|gt|lt|lte)\b/g,
-    (matchEl) => `$${matchEl}`
-  );
-  const formatedQueries = JSON.parse(queryString);
-  // Filtering
-  if (queries?.title)
-    formatedQueries.title = { $regex: queries.title, $options: "i" };
-  let queryCommand = Product.find(formatedQueries);
-  // Sorting
-  // abc,efg => [abc,efg] => abc efg
-  if (req.query.sort) {
-    const sortBy = req.query.sort?.split(",").join(" ");
-    queryCommand = queryCommand.sort(sortBy);
-  }
-  // Fields limiting
-  if (req.query.sort) {
-    const fields = req.query.fields?.split(",").join(" ");
-    queryCommand = queryCommand.select(fields);
-  }
-  // Panagation
+	const queries = { ...req.query };
+	// Exclude special fields out of query
+	const excludeFields = ["limit", "sort", "page", "fields"];
+	excludeFields.forEach((el) => delete queries[el]);
+	// Format operators for right syntax of MongoDB
+	let queryString = JSON.stringify(queries);
+	queryString = queryString.replace(
+		/\b(gte|gt|lt|lte)\b/g,
+		(matchEl) => `$${matchEl}`
+	);
+	const formatedQueries = JSON.parse(queryString);
+	// Filtering
+	if (queries?.title)
+		formatedQueries.title = { $regex: queries.title, $options: "i" };
+	let queryCommand = Product.find(formatedQueries);
+	// Sorting
+	// abc,efg => [abc,efg] => abc efg
+	if (req.query.sort) {
+		const sortBy = req.query.sort?.split(",").join(" ");
+		queryCommand = queryCommand.sort(sortBy);
+	}
+	// Fields limiting
+	if (req.query.sort) {
+		const fields = req.query.fields?.split(",").join(" ");
+		queryCommand = queryCommand.select(fields);
+	}
+	// Panagation
 
-  const page = +req.query.page || 1; // + mean: convert string to number
-  const limit = +req.query.limit || process.env.LIMIT_PRODUCTS; // number of object after call API
-  const skip = (page - 1) * limit;
-  queryCommand.skip(skip).limit(limit);
-  // Execute the query
-  // Số lượng sp thỏa mãn điều kiện !== số lượng sp trả về 1 lần gọi API
-  queryCommand
-    .exec()
-    .then(async (response) => {
-      const counts = await Product.find(formatedQueries).countDocuments();
-      return res.status(200).json({
-        success: response ? true : false,
-        counts,
-        products: response ? response : "Cannot get products",
-      });
-    })
-    .catch((err) => {
-      throw new Error(err.message);
-    });
+	const page = +req.query.page || 1; // + mean: convert string to number
+	const limit = +req.query.limit || process.env.LIMIT_PRODUCTS; // number of object after call API
+	const skip = (page - 1) * limit;
+	queryCommand.skip(skip).limit(limit);
+	// Execute the query
+	// Số lượng sp thỏa mãn điều kiện !== số lượng sp trả về 1 lần gọi API
+	queryCommand
+		.exec()
+		.then(async (response) => {
+			const counts = await Product.find(formatedQueries).countDocuments();
+			return res.status(200).json({
+				success: response ? true : false,
+				counts,
+				products: response ? response : "Cannot get products",
+			});
+		})
+		.catch((err) => {
+			throw new Error(err.message);
+		});
 });
 const updateProduct = asyncHandler(async (req, res) => {
-  const { pid } = req.params;
-  if (req.body && req.body.title) req.body.slug = slugify(req.body.title);
-  const updatedProduct = await Product.findByIdAndUpdate(pid, req.body, {
-    new: true,
-  });
-  return res.status(200).json({
-    sucess: updatedProduct ? true : false,
-    updatedProduct: updatedProduct ? updatedProduct : "Cannot update product",
-  });
+	const { pid } = req.params;
+	if (req.body && req.body.title) req.body.slug = slugify(req.body.title);
+	const updatedProduct = await Product.findByIdAndUpdate(pid, req.body, {
+		new: true,
+	});
+	return res.status(200).json({
+		success: updatedProduct ? true : false,
+		updatedProduct: updatedProduct ? updatedProduct : "Cannot update product",
+	});
 });
 const deleteProduct = asyncHandler(async (req, res) => {
-  const { pid } = req.params;
+	const { pid } = req.params;
 
-  const deleteProduct = await Product.findByIdAndDelete(pid, req.body, {
-    new: true,
-  });
-  return res.status(200).json({
-    sucess: deleteProduct ? true : false,
-    deleteProduct: deleteProduct ? deleteProduct : "Cannot delete product",
-  });
+	const deleteProduct = await Product.findByIdAndDelete(pid, req.body, {
+		new: true,
+	});
+	return res.status(200).json({
+		success: deleteProduct ? true : false,
+		deleteProduct: deleteProduct ? deleteProduct : "Cannot delete product",
+	});
 });
 const ratings = asyncHandler(async (req, res) => {
-  const { _id } = req.user;
-  const { star, comment, pid } = req.body;
+	const { _id } = req.user;
+	const { star, comment, pid } = req.body;
 
-  if (!star || !pid) throw new Error("Missing inputs");
-  const ratingProduct = await Product.findById(pid);
-  console.log(ratingProduct);
-  const isRating = ratingProduct?.ratings?.find(
-    (el) => el.postedBy.toString() === _id
-  );
-  if (isRating) {
-    // update star & comment
-    await Product.updateOne(
-      { ratings: { $elemMatch: isRating } },
-      {
-        $set: { "ratings.$.star": star, "ratings.$.comment": comment },
-      },
-      { new: true }
-    );
-  } else {
-    // add star & comment
-    const response = await Product.findByIdAndUpdate(
-      pid,
-      {
-        $push: {
-          ratings: {
-            star,
-            comment,
-            postedBy: _id,
-          },
-        },
-      },
-      { new: true }
-    );
-  }
-  // sum total rating
-  const updatedProduct = await Product.findById(pid);
-  const ratingCount = updatedProduct.ratings.length;
-  const sumRatings = updatedProduct.ratings.reduce(
-    (sum, el) => sum + +el.star,
-    0
-  );
-  updatedProduct.totalRatings =
-    Math.round((sumRatings * 10) / ratingCount) / 10;
-  await updatedProduct.save();
-  return res.status(200).json({
-    status: true,
-    updatedProduct,
-  });
+	if (!star || !pid) throw new Error("Missing inputs");
+	const ratingProduct = await Product.findById(pid);
+	console.log(ratingProduct);
+	const isRating = ratingProduct?.ratings?.find(
+		(el) => el.postedBy.toString() === _id
+	);
+	if (isRating) {
+		// update star & comment
+		await Product.updateOne(
+			{ ratings: { $elemMatch: isRating } },
+			{
+				$set: { "ratings.$.star": star, "ratings.$.comment": comment },
+			},
+			{ new: true }
+		);
+	} else {
+		// add star & comment
+		const response = await Product.findByIdAndUpdate(
+			pid,
+			{
+				$push: {
+					ratings: {
+						star,
+						comment,
+						postedBy: _id,
+					},
+				},
+			},
+			{ new: true }
+		);
+	}
+	// sum total rating
+	const updatedProduct = await Product.findById(pid);
+	const ratingCount = updatedProduct.ratings.length;
+	const sumRatings = updatedProduct.ratings.reduce(
+		(sum, el) => sum + +el.star,
+		0
+	);
+	updatedProduct.totalRatings =
+		Math.round((sumRatings * 10) / ratingCount) / 10;
+	await updatedProduct.save();
+	return res.status(200).json({
+		status: true,
+		updatedProduct,
+	});
 });
 const uploadImagesProduct = asyncHandler(async (req, res) => {
-  const { pid } = req.params;
-  const imagesPath = req.files.map((el) => el.path);
-  if (!req.files) throw new Error("Missing inputs");
-  const response = await Product.findByIdAndUpdate(
-    pid,
-    { $push: { images: { $each: imagesPath } } },
-    { new: true }
-  );
-  return res.status(200).json({
-    status: response ? true : false,
-    updatedProduct: response ? response : "Cannot upload images product",
-  });
+	const { pid } = req.params;
+	const imagesPath = req.files.map((el) => el.path);
+	if (!req.files) throw new Error("Missing inputs");
+	const response = await Product.findByIdAndUpdate(
+		pid,
+		{ $push: { images: { $each: imagesPath } } },
+		{ new: true }
+	);
+	return res.status(200).json({
+		status: response ? true : false,
+		updatedProduct: response ? response : "Cannot upload images product",
+	});
 });
 
 module.exports = {
-  createProduct,
-  getProduct,
-  getAllProduct,
-  updateProduct,
-  deleteProduct,
-  ratings,
-  uploadImagesProduct,
+	createProduct,
+	getProduct,
+	getAllProduct,
+	updateProduct,
+	deleteProduct,
+	ratings,
+	uploadImagesProduct,
 };
