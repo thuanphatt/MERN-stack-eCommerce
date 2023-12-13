481690178c01ecbbdcee40cfb9c1e406908ca082
thuanphatt <thphatt@gmail.com>
2023-12-13 17:13:06 +0700

    update

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(
 	cors({
-		origin: process.env.CLIENT_URL || "https://greentp-client-thuanphat.vercel.app/",
+		origin: process.env.CLIENT_URL || "https://greentp.vercel.app/",
 		methods: ["POST", "PUT", "GET", "DELETE"],
 		credentials: true,
 	})
